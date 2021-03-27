52adbf718bb7e2b2d78b851487a3f3d02869a945
Yuzuki9 <yuzuki96cherry@gmail.com>
2021-03-27 20:55:59 +0900

    getter

--- resources/views/front/posts/index.blade.php
+++ resources/views/front/posts/index.blade.php
@@ -15,7 +15,7 @@ $title = '投稿一覧';
         <table class="table">
             @foreach($posts as $post)
                 <tr>
-                    <td>{{ $post->published_at->format('Y年m月d日') }}</td>
+                    <td>{{ $post->published_format }}</td>  {{-- ゲッターを使ってフォーマットを統一、PostモデルのgetPublishedFormatAttrobuteを表示 --}}
                     <td>{!! link_to_route('front.posts.show', $post->title, $post) !!}</td>  <!-- link_to_routeは最初にインストールした、「Laravel Collective」の機能 -->
                 </tr>
             @endforeach

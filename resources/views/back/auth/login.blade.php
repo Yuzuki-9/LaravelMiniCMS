cfa9efafe51f1d03198ef7f4248ea05c4f454104
Yuzuki9 <yuzuki96cherry@gmail.com>
2021-03-27 23:06:51 +0900

    login and logout

--- resources/views/back/auth/login.blade.php
+++ resources/views/back/auth/login.blade.php
@@ -8,44 +8,40 @@
 </head>
 <body>
 <div id="app">
-    <div class="container">
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">ログイン</div>
-                    <div class="card-body">
-                        {{ Form::open(['route' => 'login']) }}
-                            <div class="form-group">
-                                {{ Form::label('name', 'ユーザー名') }}
-                                {{ Form::text('name', null, [
-                                    'class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : '')
-                                ]) }}
-                                @error('name')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                {{ Form::label('password', 'パスワード') }}
-                                {{ Form::password('password', [
-                                    'class' => 'form-control' . ($errors->has('password') ? ' is-invalid' : '')
-                                ]) }}
-                                @error('password')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                            <div>
-                                <button type="submit" class="btn btn-primary">ログイン</button>
-                            </div>
-                        {{ Form::close() }}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">ログイン</div>
+                    <div class="card-body">
+                        {{ Form::open(['route' => 'login']) }}
+                            <div class="form-group">
+                                {{ Form::label('name', 'ユーザー名') }}
+                                {{ Form::text('name', null, [
+                                    'class' => 'form-control' . ($errors->has('name') ? 'is-invalid' : '')
+                                ]) }}
+                                @error('name')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                {{ Form::label('password', 'パスワード') }}
+                                {{ Form::password('password', [
+                                    'class' => 'form-control' . ($errors->has('password') ? 'is-invalid' : '')
+                                ]) }}
+                                @error('password')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div>
+                                <button type="submit" class="btn btn-primary">ログイン</button>
+                            </div>
+                        {{ Form::close() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
